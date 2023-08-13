ac5e8fe00d0a36785d103116d98312011350837c
husnainakram09 <husnainakram7860@gmail.com>
2023-08-13 22:16:31 +0500

    fx: error

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-// import { ExpressAdapter } from '@nestjs/platform-express';
-// import * as express from 'express';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
 import * as multer from 'multer';
-
 
 async function bootstrap() {
   // const app = await NestFactory.create(AppModule);
-  const app = await NestFactory.create(AppModule);
-  // new ExpressAdapter(expressApp),
+  const expressApp = express();
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressApp),
+  );
   // Multer configuration
   const storage = multer.memoryStorage(); // Store images in memory
   const upload = multer({ storage });
