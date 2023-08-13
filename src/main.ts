e153564daa9eb6d178560c12d9984302e2ef662b
husnainakram09 <husnainakram7860@gmail.com>
2023-08-13 21:41:10 +0500

    fx: error

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import * as multer from 'multer';
 
 async function bootstrap() {
   // const app = await NestFactory.create(AppModule);
-  const expressApp: express.Application = express();
+  const expressApp: any = express();
   const app = await NestFactory.create(
     AppModule,
     new ExpressAdapter(expressApp),
